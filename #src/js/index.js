c92b1e8502bd180a7b10422da0467067ef2ecefc
Marina Dariy <dariy.mvs@gmail.com>
2021-08-02 17:43:33 +0300

    f

--- #src/js/index.js
+++ #src/js/index.js
@@ -83,6 +83,7 @@ $( document ).ready( () => {
   
   pagination('.portfolio__list', '.portfolio__item', 6);
   pagination('.blog-main__list', '.blog-home__item', 5);
+  pagination('.catalog__list', '.catalog__item', 5);
 
 
   // popup для портфолио
@@ -99,4 +100,92 @@ $( document ).ready( () => {
       }
     }
   } );
+
+  // показ поля комментов 
+
+  $('.comments__write').on('click', () => {
+    $('.comments__write_form').toggleClass('formHidden');
+  });
+
+  // слайдер для страницы продукта
+
+  $('.product__big_slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    fade: true,
+    asNavFor: '.product__slider'
+  });
+  $('.product__slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    vertical: true,
+    asNavFor: '.product__big_slider',
+    //dots: true,
+    centerMode: false,
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          vertical: false,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        }
+      }, 
+      {
+        breakpoint: 768,
+        settings: {
+          vertical: false,
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      }
+    ]
+  });
+
+  // слайдер похожих работ
+
+  $( '.product__anotherlist' ).slick( {
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  } );
+
+  // табы страницы продукта 
+
+  $('.product__tabs_item').on('click', (event) => {
+    let target = event.target;
+    if (target.classList.contains('active')) {
+      return;
+    } else {
+      let tab = target.closest('.product__tabs_item');
+      let content = document.querySelector('.product__tabs_content');
+      $('.active').removeClass('active');
+      tab.classList.add('active');
+      console.log(tab.dataset.tab);
+      if (tab.dataset.tab === 'description') {
+        console.log('a');
+        content.innerHTML = '<p>Компания «Давыдов и Ко» активно развивается на рынке мебели и изделий для интерьера из натурального дерева Санкт-Петербурга. За годы своей деятельности мы сформировали коллектив высококвалифицированных сотрудников и развили партнёрские отношения с надёжными поставщиками. Развитие бизнеса и охват новых рынков побуждает нас расширять штат дизайнеров и архитекторов, а также строить доверительные отношения с производителями натуральной древесины, фурнитуры и лакокрасочных материалов.</p>'
+      } else if (tab.dataset.tab === 'color') {
+        console.log('b');
+        content.innerHTML = '<p>Контент цветов</p>'
+      } else if (tab.dataset.tab === 'review') {
+        console.log('c');
+        content.innerHTML = '<p>Контент отзывов</p>'
+      } else if (tab.dataset.tab === 'video') {
+        console.log('d');
+        content.innerHTML = '<p>Контент video</p>'
+      } 
+    }
+  })
 } );
